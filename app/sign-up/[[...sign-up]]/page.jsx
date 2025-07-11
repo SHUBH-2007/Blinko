4d2e4cb95d93b02b1405444e11a00fc2d6e947aa
SHUBH-2007 <shubh20072001@gmail.com>
2025-07-11 14:20:35 +0530

    Clerk setup and auth setuped

---
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignInPage() {
+    return (
+    <div className="flex flex-col items-center justify-center h-screen">
+    <SignUp />
+    </div>
+    );
+}
